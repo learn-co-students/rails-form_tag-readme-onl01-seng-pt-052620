39a3a693bcdfb751ad51bf05e800198843f24dbd
DmitriyMalayev <dmitriy.malayev@gmail.com>
2020-09-30 22:53:37 -0400

    Done.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   end
   
   def create
-    Post.create(title: params[:post][:title], description: params[:post][:title])
+    Post.create(title: params[:post][:title], description: params[:post][:description])
     redirect_to posts_path
   end
   
